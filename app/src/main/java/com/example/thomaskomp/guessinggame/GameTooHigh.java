cba4718adccaa7c0e6c42ecf4c6693ce9b969b34
Thomas William Komp <twkomp@gmail.com>
2018-06-15 13:38:56 -0400

    added guis

---
+++ app/src/main/java/com/example/thomaskomp/guessinggame/GameTooHigh.java
@@ -0,0 +1,13 @@
+package com.example.thomaskomp.guessinggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameTooHigh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_too_high);
+    }
+}
